d12e935ff60f1394d8e50a1297846d41195aecf1
marcelvanwyk <marcelvw@runninghill.co.za>
2022-04-12 11:27:48 +0200

    Changed age analysis logic to iterate through transactions

--- invoiceLogic.js
+++ invoiceLogic.js
@@ -179,7 +179,7 @@ const getPaymentsTotal = exports.getInvoicePaymentsTotal = (payments) => {
 };
 
 
-exports.ageAnalysis = (clientBalance, invoice) => {
+exports.ageAnalysis = (transactions) => {
     const ageAnalysis = {
         balance: 0,
         current: 0,
@@ -190,32 +190,35 @@ exports.ageAnalysis = (clientBalance, invoice) => {
         hundredAndFiftyDaysAndAfter: 0,
     };
 
-    const totals = calculateTotals(invoice.items, invoice.settingTaxRate, invoice.settingTaxType, invoice.discountPercentage, invoice.discountAmount);
-    const paymentsTotal = getPaymentsTotal(invoice.payments);
-    const invoiceTotal = totals.total;
-    const balance = new BigNumber(invoiceTotal).minus(new BigNumber(paymentsTotal));
-    ageAnalysis.balance = new BigNumber(clientBalance).plus(balance).toNumber();
-    if (balance.toNumber() !== 0) {
-        const dueDate = invoiceDueDate(invoice);
-        const todaysDate = moment();
-        const thirtyDaysFromDueDate = dueDate.clone().add(30, 'days');
-        const sixtyDaysFromDueDate = dueDate.clone().add(60, 'days');
-        const ninetyDaysFromDueDate = dueDate.clone().add(90, 'days');
-        const hundredAndTwentyDaysFromDueDate = dueDate.clone().add(120, 'days');
-        const hundredAndFiftyDaysFromDueDate = dueDate.clone().add(150, 'days');
-        if (todaysDate.isAfter(thirtyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(sixtyDaysFromDueDate, 'days')) {
-            ageAnalysis.thirtyDays = new BigNumber(ageAnalysis.thirtyDays).plus(new BigNumber(balance)).toNumber();
-        } else if (todaysDate.isAfter(sixtyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(ninetyDaysFromDueDate, 'days')) {
-            ageAnalysis.sixtyDays = new BigNumber(ageAnalysis.sixtyDays).plus(new BigNumber(balance)).toNumber();
-        } else if (todaysDate.isAfter(ninetyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(hundredAndTwentyDaysFromDueDate, 'days')) {
-            ageAnalysis.ninetyDays = new BigNumber(ageAnalysis.ninetyDays).plus(new BigNumber(balance)).toNumber();
-        } else if (todaysDate.isAfter(hundredAndTwentyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(hundredAndFiftyDaysFromDueDate, 'days')) {
-            ageAnalysis.hundredAndTwentyDays = new BigNumber(ageAnalysis.hundredAndTwentyDays).plus(new BigNumber(balance)).toNumber();
-        } else if (todaysDate.isAfter(hundredAndFiftyDaysFromDueDate, 'days')) {
-            ageAnalysis.hundredAndFiftyDaysAndAfter = new BigNumber(ageAnalysis.hundredAndFiftyDaysAndAfter).plus(new BigNumber(balance)).toNumber();
-        } else {
-            ageAnalysis.current = new BigNumber(ageAnalysis.current).plus(new BigNumber(balance)).toNumber();
+    for (const transaction of transactions){
+        const totals = transaction.isQuote ? {total:0} : calculateTotals(transaction.items, transaction.settingTaxRate, transaction.settingTaxType, transaction.discountPercentage, transaction.discountAmount);
+        const paymentsTotal = getPaymentsTotal(transaction.payments);
+        const transactionTotal = totals.total;
+        const balance = new BigNumber(transactionTotal).minus(new BigNumber(paymentsTotal));
+        ageAnalysis.balance = new BigNumber(ageAnalysis.balance).plus(balance).toNumber();
+        if (balance.toNumber() !== 0) {
+            const dueDate = invoiceDueDate(transaction);
+            const todaysDate = moment();
+            const thirtyDaysFromDueDate = dueDate.clone().add(30, 'days');
+            const sixtyDaysFromDueDate = dueDate.clone().add(60, 'days');
+            const ninetyDaysFromDueDate = dueDate.clone().add(90, 'days');
+            const hundredAndTwentyDaysFromDueDate = dueDate.clone().add(120, 'days');
+            const hundredAndFiftyDaysFromDueDate = dueDate.clone().add(150, 'days');
+            if (todaysDate.isAfter(thirtyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(sixtyDaysFromDueDate, 'days')) {
+                ageAnalysis.thirtyDays = new BigNumber(ageAnalysis.thirtyDays).plus(new BigNumber(balance)).toNumber();
+            } else if (todaysDate.isAfter(sixtyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(ninetyDaysFromDueDate, 'days')) {
+                ageAnalysis.sixtyDays = new BigNumber(ageAnalysis.sixtyDays).plus(new BigNumber(balance)).toNumber();
+            } else if (todaysDate.isAfter(ninetyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(hundredAndTwentyDaysFromDueDate, 'days')) {
+                ageAnalysis.ninetyDays = new BigNumber(ageAnalysis.ninetyDays).plus(new BigNumber(balance)).toNumber();
+            } else if (todaysDate.isAfter(hundredAndTwentyDaysFromDueDate, 'days') && todaysDate.isSameOrBefore(hundredAndFiftyDaysFromDueDate, 'days')) {
+                ageAnalysis.hundredAndTwentyDays = new BigNumber(ageAnalysis.hundredAndTwentyDays).plus(new BigNumber(balance)).toNumber();
+            } else if (todaysDate.isAfter(hundredAndFiftyDaysFromDueDate, 'days')) {
+                ageAnalysis.hundredAndFiftyDaysAndAfter = new BigNumber(ageAnalysis.hundredAndFiftyDaysAndAfter).plus(new BigNumber(balance)).toNumber();
+            } else {
+                ageAnalysis.current = new BigNumber(ageAnalysis.current).plus(new BigNumber(balance)).toNumber();
+            }
         }
+
     }
 
     return ageAnalysis;
